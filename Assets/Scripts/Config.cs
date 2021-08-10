33d9138cd82a0d7039465c8c818ef1300013663a
DESKTOP-CM4IVQR\Dimas <dwisodewo@gmail.com>
2021-08-11 01:49:32 +0700

    Remove redundant text in JSON data, Add app icon

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public static class Config
 {
-    public static string APP_VERSION = "v.1.0.1.I.17";
+    public static string APP_VERSION = "v.1.0.1.III.19";
 
     public const string RESUME_TEXT = "Resume";
     public const string PAUSE_TEXT = "Pause";
